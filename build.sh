3549b3287b14ca965ede57857f0ab0b87779e5bb
fimreal <6913651@qq.com>
2023-04-28 16:02:31 +0800

    build.sh 添加 gost 二进制文件下载更新

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ DESCRIPTION="gost"
 HOME_URL="Module_gost.asp"
 TAGS="代理转发服务"
 AUTHOR="fimreal"
+ARCH=linux_armv7    # AC86U
 
 # Check and include base
 DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
@@ -20,4 +21,20 @@ DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 cd $DIR
 
 # do something here
+set -e
+
+assest_url=$(
+    curl -sL 'https://api.github.com/repos/go-gost/gost/releases?per_page=1' | \
+    awk -F\" -v ARCH="${ARCH}" '/http/{if($0 ~ ARCH) print $(NF-1)}'
+)
+echo download: ${assest_url}
+
+# use proxy
+# curl -L https://cfdown.2fw.top/${assest_url} -o gost.tgz
+curl -L ${assest_url} -o gostbin.tgz
+
+tar xf gostbin.tgz -C gost/bin/ gost
+
+rm gostbin.tgz
+
 do_build_result
