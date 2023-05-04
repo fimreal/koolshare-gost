a1bc9b13974a64f1c9c3932fda69dd5140829e7a
fimreal <6913651@qq.com>
2023-05-04 09:29:09 +0800

    手动打包 v0.1

---
+++ config.json.js
@@ -0,0 +1,12 @@
+{
+"version":"0.1",
+"md5":"8c74f035b533bea0efc37b90997e0288",
+"home_url":"Module_gost.asp",
+"title":"gost",
+"description":"gost",
+"tags":"代理转发服务",
+"author":"fimreal",
+"link":"",
+"changelog":"",
+"build_date":"2023-05-04_09:27:37"
+}
